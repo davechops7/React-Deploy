d272c6e2db27dbf3473bb96f7375a0ffc32d10db
David Kavanagh <dkavanagh@junpier.net>
2020-07-01 12:21:50 +0100

    Fixing bg setting

--- src/App.js
+++ src/App.js
@@ -20,42 +20,42 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className="App">
-      <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-         <h1>
-           Chops Social Media Accounts, enjoy!
-        </h1>
-          <a
-            className="App-link"
-            href="https://www.facebook.com/David.kavana7"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Facebook
-        </a>
-          <a
-            className="App-link"
-            href="https://www.instagram.com/chops_and_dogs/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Instagram
-        </a>
-          <a
-            className="App-link"
-            href="https://twitter.com/Dave_kav"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Twitter
-        </a>
       <div style={{ background: this.state.color }}>
-        <button onClick={this.changeColor}>
-          Change bg color
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1>
+              Chops Social Media Accounts, enjoy!
+        </h1>
+            <a
+              className="App-link"
+              href="https://www.facebook.com/David.kavana7"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Facebook
+        </a>
+            <a
+              className="App-link"
+              href="https://www.instagram.com/chops_and_dogs/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Instagram
+        </a>
+            <a
+              className="App-link"
+              href="https://twitter.com/Dave_kav"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Twitter
+        </a>
+            <button onClick={this.changeColor}>
+              Change bg color
 				</button>
-      </div>
-        </header>
+          </header>
+        </div>
       </div>
     );
   }
