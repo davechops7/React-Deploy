bf165de2b0adcca914a8532cbda50375c59bbedc
David Kavanagh <dkavanagh@junpier.net>
2020-07-01 14:37:55 +0100

    Update div header

--- src/App.js
+++ src/App.js
@@ -20,42 +20,40 @@ class App extends React.Component {
 
   render() {
     return (
-      <div style={{ background: this.state.color }}>
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1>
-              Chops Social Media Accounts, enjoy!
+      <div className="App" style={{ background: this.state.color }}>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>
+            Chops Social Media Accounts, enjoy!
         </h1>
-            <a
-              className="App-link"
-              href="https://www.facebook.com/David.kavana7"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Facebook
+          <a
+            className="App-link"
+            href="https://www.facebook.com/David.kavana7"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Facebook
         </a>
-            <a
-              className="App-link"
-              href="https://www.instagram.com/chops_and_dogs/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Instagram
+          <a
+            className="App-link"
+            href="https://www.instagram.com/chops_and_dogs/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Instagram
         </a>
-            <a
-              className="App-link"
-              href="https://twitter.com/Dave_kav"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Twitter
+          <a
+            className="App-link"
+            href="https://twitter.com/Dave_kav"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Twitter
         </a>
-            <button onClick={this.changeColor}>
-              Change bg color
+          <button onClick={this.changeColor}>
+            Change bg color
 				</button>
-          </header>
-        </div>
+        </header>
       </div>
     );
   }
